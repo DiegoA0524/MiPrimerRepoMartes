2d33f5df6c8cfcec60fec398c51e7cbff2a76aa6
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-02-27 20:29:53 -0500

    mensaje

---
+++ main.py
@@ -0,0 +1,14 @@
+userDB="hidrotech123"
+passwordBD="admin123*"
+
+print('software tech hidroituango')
+print('**************************')
+userName=input('Digite tu usuario: ')
+userPassword=input('Digita tu contraseña: ')
+
+'''if userDB==userName and passwordBD==userPassword:
+    print("exito en su usuario")
+else:
+    print("fallaste")'''
+
+'''print("exito en su usuario") if userDB==userName and passwordBD==userPassword else print("fallaste")'''
